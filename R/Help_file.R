2007d6bad08a1d63d92ff8ea5d5afb6f847d8e7c
Gard Kroken <gard.kroken@fiskeridirektoratet.no>
2022-11-10 09:22:57 +0100

    initial commit

---
+++ R/Help_file.R
@@ -0,0 +1,13 @@
+#' A package to test github (TestPackage)
+#'
+#' For use with github and publishing
+#'
+#'
+#' @docType package
+#'
+#' @author Gard Kroken \email{yourself@gmail.com}
+#'
+#' @name TestPackage
+#'
+#' @import dplyr
+NULL
---
+++ R/add.R
@@ -0,0 +1,13 @@
+
+#' Adds two numbers together
+#'
+#' @param a Numeric variable
+#' @param b Numeric variable
+#'
+#' @return Numeric sum of variables
+#'
+#' @export
+add <- function(a, b){
+  return(a+b)
+}
+
